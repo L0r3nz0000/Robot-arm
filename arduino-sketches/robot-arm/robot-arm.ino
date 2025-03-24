5abe3309b57396df5d4475b41419811033ed22e6
L0r3nz0000 <lorenzobefera0@gmail.com>
2025-03-24 07:38:09 +0100

    _

--- arduino-sketches/robot-arm/robot-arm.ino
+++ arduino-sketches/robot-arm/robot-arm.ino
@@ -1,9 +1,10 @@
 #include <AccelStepper.h>
 #include <math.h>
+#include <stdlib.h>
 
-/ *    MODIFICARE I NUMERI DEI PIN   * /
-#define STEPPER_0_STEP 0
-#define STEPPER_0_DIR 0
+/*    MODIFICARE I NUMERI DEI PIN   */
+#define STEPPER_0_STEP 8
+#define STEPPER_0_DIR 3
 #define STEPPER_1_STEP 0
 #define STEPPER_1_DIR 0
 #define STEPPER_2_STEP 0
@@ -29,10 +30,10 @@ struct ArmPosition {
 
 // Configurazione dei driver
 const float angle_for_step = 1.8;  // 1.8° per step con i nema 17
-const int microsteps = 4;  // 1=full step, 2=half step, 4=quarter step.... 
+const int microsteps = 8;  // 1=full step, 2=half step, 4=quarter step.... 
 const float steps_per_degree = (1.0 / angle_for_step) * microsteps; // Step per grado
-long max_speed = 2000;  // Velocità massima (step/secondo)
-long max_accel = 5000;  // Accelerazione (step/secondo²)
+long max_speed = 200;  // Velocità massima (step/secondo)
+long max_accel = 200;  // Accelerazione (step/secondo²)
 
 // Angoli dei motori
 ArmPosition angles = (ArmPosition) {0, 0, 0, 0};
@@ -50,12 +51,12 @@ bool isValidInt(String str, int &num) {
 
 bool isValidFloat(String str, float &num) {
   char *endptr;
-  num = strtof(str.c_str(), &endptr);
+  num = atof(str.c_str()); // Controllare errori , &endptr);
   
   return (*endptr == '\0'); // Controlla che tutta la stringa sia stata convertita
 }
 
-ArmPosition calcolaAngoli(float X, float Y, float Z, float R, float L1, float L2) {
+ArmPosition calcolaAngoli(float X, float Y, float Z, float T3, float L1, float L2) {
   ArmPosition angoli;
 
   // Calcola angolo della base
@@ -77,24 +78,24 @@ ArmPosition calcolaAngoli(float X, float Y, float Z, float R, float L1, float L2
 
   // Angolo theta2
   float cosTheta2 = (L1 * L1 + L2 * L2 - R * R) / (2 * L1 * L2);
-  angoli.theta2 = acos(cosTheta2) * 180.0 / M_PI;ù
+  angoli.theta2 = acos(cosTheta2) * 180.0 / M_PI;
 
   // Angolo theta3
-  angoli.theta3 = R;
+  angoli.theta3 = T3;
 
   return angoli;
 }
 
 void setAngle(AccelStepper &s, float position, float degrees) {
   long target_steps = degrees * steps_per_degree - position; // Converte i gradi in step
-  s.moveTo(target_steps);
+  s.move(target_steps);
 }
 
 void setArmPosition(ArmPosition angles) {
-  setAngle(steppers[0], angles[0], angles.theta0);
-  setAngle(steppers[1], angles[1], angles.theta1);
-  setAngle(steppers[2], angles[2], angles.theta2);
-  setAngle(steèèers[3], angles[3], angles.theta3);
+  setAngle(steppers[0], angles.theta0, angles.theta0);
+  setAngle(steppers[1], angles.theta1, angles.theta1);
+  setAngle(steppers[2], angles.theta2, angles.theta2);
+  setAngle(steppers[3], angles.theta3, angles.theta3);
 }
 
 String command = "";
@@ -105,19 +106,24 @@ bool somethingIsMoving = false;
 
 void setup() {
   for (int i = 0; i < NUM_MOTORS; i++) {
-    stepper0.setMaxSpeed(max_speed);
-    stepper0.setAcceleration(max_accel);
+    steppers[0].setMaxSpeed(max_speed);
+    steppers[0].setAcceleration(max_accel);
   }
   Serial.begin(115200);
 }
 
 void loop() {
-  
+  //setAngle(steppers[0], angles.theta0, 0);
+  steppers[0].moveTo(90);
+  while (steppers[0].distanceToGo() != 0) { steppers[0].run(); }
+  //setAngle(steppers[0], angles.theta0, 90);
+  steppers[0].moveTo(0);
+  while (steppers[0].distanceToGo() != 0) { steppers[0].run(); }
 
-  for (int i = 0; i < NUM_MOTORS; i++) {
+  /*for (int i = 0; i < NUM_MOTORS; i++) {
     if (steppers[i].distanceToGo() != 0) {
       steppers[i].run();
       somethingIsMoving = true;
     }
-  }
+  }*/
 }
